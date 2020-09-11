fd7788ffa076cc0952313d45157699629bb06738
Evan Stoddard <evanstoddard23@gmail.com>
2020-09-10 19:46:02 -0500

    init

---
+++ Formula/terragrunt.rb
@@ -0,0 +1,18 @@
+class Terragrunt < Formula
+  desc "Thin wrapper for Terraform e.g. for locking state"
+  homepage "https://github.com/gruntwork-io/terragrunt"
+  url "https://github.com/gruntwork-io/terragrunt/archive/v0.24.1.tar.gz"
+  sha256 "c33b7373b746419dfe47702101bb39a0f14a60504b1d9a0e1b16f4e0d3cd2539"
+  license "MIT"
+
+  depends_on "go" => :build
+  depends_on "terraform" => :optional
+
+  def install
+    system "go", "build", "-ldflags", "-X main.VERSION=v#{version}", *std_go_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/terragrunt --version")
+  end
+end
