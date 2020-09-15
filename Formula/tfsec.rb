6695ee6f5bcb83367d36005047bf69f9394205f1
Evan Stoddard <estoddard@opploans.com>
2020-09-14 20:03:40 -0500

    try adding tfsec

---
+++ Formula/tfsec.rb
@@ -0,0 +1,17 @@
+class Tfsec < Formula
+  desc "Static analysis powered security scanner for your terraform code"
+  homepage "https://github.com/liamg/tfsec"
+  url "https://github.com/liamg/tfsec/archive/v0.25.0.tar.gz"
+  sha256 "33a458c7b06af3c399c49859452bf6f98b6da8e168cb01337f200afbea270778"
+
+  depends_on "go" => :build
+
+  def install
+	  system "scripts/install.sh", "v#{version}"
+    bin.install "tfsec" => "tfsec"
+  end
+
+  test do
+    system "#{bin}/tfsec", "--help"
+  end
+end
