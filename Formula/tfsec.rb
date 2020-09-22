9d4b0c0dac5245d86e4571cc8c0a705ff2d36cae
Evan Stoddard <evanstoddard23@gmail.com>
2020-09-22 18:20:16 +0000

    tfsec 0.26.0

--- Formula/tfsec.rb
+++ Formula/tfsec.rb
@@ -1,8 +1,8 @@
 class Tfsec < Formula
   desc "Static analysis powered security scanner for your terraform code"
   homepage "https://github.com/liamg/tfsec"
-  url "https://github.com/liamg/tfsec/archive/v0.25.0.tar.gz"
-  sha256 "33a458c7b06af3c399c49859452bf6f98b6da8e168cb01337f200afbea270778"
+  url "https://github.com/liamg/tfsec/archive/v0.26.0.tar.gz"
+  sha256 "a54f8f4e5488590cd1f3d0cb8dd8d5777bd6719ae0cf2b398f1619f8f500c985"
 
   depends_on "go" => :build
 
