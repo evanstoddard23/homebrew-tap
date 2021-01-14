1b3c938593d0ce45b41f297d8f8e1350505f604d
Evan Stoddard <evanstoddard23@gmail.com>
2021-01-14 14:46:52 -0600

    Update terragrunt.rb

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -4,6 +4,13 @@ class Terragrunt < Formula
   url "https://github.com/gruntwork-io/terragrunt/archive/v0.27.1.tar.gz"
   sha256 "5cc13d6556b9206335be97a5e8f163c85e2097a44b352c284efb369ea6c0e446"
   license "MIT"
+  
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "74e29b1d404c975c8ccaabe9279a3e411541394dafe7748d99cf249c7804cfe2" => :big_sur
+    sha256 "02fccf8e475ee846f2a243f401528a739864b7c5389a41818015e4257208ccb3" => :catalina
+    sha256 "f25d564b4fad246c6e3e4425070a9e6dcd7d8721387852871f4bee7be6e0148f" => :mojave
+  end
 
   depends_on "go" => :build
   depends_on "tfenv" => :recommended
